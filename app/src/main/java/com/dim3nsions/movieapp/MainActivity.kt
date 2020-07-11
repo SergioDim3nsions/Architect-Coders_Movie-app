579b2c786db05d24204826d9c2f1546af42eb9ad
Sergio Vasco <sergiovascoportillo@gmail.com>
2020-07-11 14:00:47 +0200

    create project

---
+++ app/src/main/java/com/dim3nsions/movieapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.dim3nsions.movieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dim3nsions.movieapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/dim3nsions/movieapp/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.dim3nsions.movieapp.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
