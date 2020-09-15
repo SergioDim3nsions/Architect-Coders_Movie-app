4233f4270b4515a46355df4d60a4743506550211
Sergio Vasco <sergiovascoportillo@gmail.com>
2020-09-15 18:30:34 +0200

    Update TypeConverter

--- app/src/main/java/com/dim3nsions/movieapp/db/Converters.kt
+++ app/src/main/java/com/dim3nsions/movieapp/db/Converters.kt
@@ -4,15 +4,10 @@ import androidx.room.TypeConverter
 import com.dim3nsions.movieapp.db.model.DBCast
 import com.dim3nsions.movieapp.db.model.DBCompany
 import com.dim3nsions.movieapp.db.model.DBGenre
+import com.google.gson.Gson
 
 @Suppress("UNCHECKED_CAST")
 class Converters {
-
-    @TypeConverter
-    fun fromListIntToInt(list: List<Int> = listOf()): String = list.toString()
-
-    @TypeConverter
-    fun fromIntToListInt(listString: String): List<Int> = listString.toList() as List<Int>
 
     @TypeConverter
     fun fromListGenreToString(list: List<DBGenre>): String = list.toString()
@@ -21,6 +16,7 @@ class Converters {
     fun fromStringToListGenre(listString: String): List<DBGenre> =
         listString.toList() as List<DBGenre>
 
+
     @TypeConverter
     fun fromListCompanyToString(list: List<DBCompany>): String = list.toString()
 
@@ -28,9 +24,17 @@ class Converters {
     fun fromStringToListCompany(listString: String): List<DBCompany> =
         listString.toList() as List<DBCompany>
 
+
     @TypeConverter
     fun fromListCastToString(list: List<DBCast>): String = list.toString()
 
     @TypeConverter
     fun fromStringToListCast(listString: String): List<DBCast> = listString.toList() as List<DBCast>
+
+
+    @TypeConverter
+    fun fromListIntToJson(value: List<Int>) = Gson().toJson(value)
+
+    @TypeConverter
+    fun jsonToListInt(value: String) = Gson().fromJson(value, Array<Int>::class.java).toList()
 }
